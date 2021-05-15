987abc4da242391aecfca02ccaee867bfb9e2319
c88032111 <c88032111@gmail.com>
2021-05-16 01:35:38 +0800

    fix bug

--- consensus/gdtuash/consensus.go
+++ consensus/gdtuash/consensus.go
@@ -320,19 +320,20 @@ func (gdtuash *Gdtuash) CalcDifficulty(chain consensus.ChainHeaderReader, time u
 // the difficulty that a new block should have when created at time
 // given the parent block's time and difficulty.
 func CalcDifficulty(config *params.ChainConfig, time uint64, parent *types.Header) *big.Int {
-	next := new(big.Int).Add(parent.Number, big1)
-	switch {
-	case config.IsMuirGlacier(next):
-		return calcDifficultyEip2384(time, parent)
-	case config.IsConstantinople(next):
-		return calcDifficultyConstantinople(time, parent)
-	case config.IsByzantium(next):
-		return calcDifficultyByzantium(time, parent)
-	case config.IsHomestead(next):
-		return calcDifficultyHomestead(time, parent)
-	default:
-		return calcDifficultyFrontier(time, parent)
-	}
+	//next := new(big.Int).Add(parent.Number, big1)
+	//switch {
+	//case config.IsMuirGlacier(next):
+	//	return calcDifficultyEip2384(time, parent)
+	//case config.IsConstantinople(next):
+	//	return calcDifficultyConstantinople(time, parent)
+	//case config.IsByzantium(next):
+	//	return calcDifficultyByzantium(time, parent)
+	//case config.IsHomestead(next):
+	//	return calcDifficultyHomestead(time, parent)
+	//default:
+	//	return calcDifficultyFrontier(time, parent)
+	//}
+	return calcDifficultyFrontier(time, parent)
 }
 
 // Some weird constants to avoid constant memory allocs for them.
